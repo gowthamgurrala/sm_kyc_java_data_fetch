821fbf32bb7c0fa7263f7a59c370131a172a13fc
raushan kumar <raushan.kumar1@scoreme.in>
2021-08-17 12:07:57 +0530

    change add log service

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -10,7 +10,7 @@ import okhttp3.*;
 
 import java.io.IOException;
 import java.lang.reflect.Type;
-
+import java.util.HashMap;
 import java.util.Map;
 
 
@@ -33,21 +33,39 @@ public class DemoApplication implements RequestHandler<SQSEvent, Void>{
 				.addHeader("Content-Type", "application/json")
 				.build();
 		Response response = client.newCall(request).execute();
+		
+		
+		
+		
 		if(response!=null){
+			OkHttpClient client1 = new OkHttpClient().newBuilder()
+					  .build();
+					MediaType mediaType1 = MediaType.parse("application/json");
+					RequestBody body1 = RequestBody.create(mediaType1, "{\"referenceId\":\"1683f278-8502-401a-bc1e-d20157881afe\",\n\"productType\":\"PAN\",\n\"responseCode\":\"2000\"}");
+					Request request1 = new Request.Builder()
+					  .url("https://sm-kyc-quality.scoreme.in/kyc/save/smkycvendorrequestdetails")
+					  .method("POST", body1)
+					  .addHeader("ClientId", "85a55d204c3fc467764f1985205aaa67")
+					  .addHeader("ClientSecret", "4ac8ec0816af87f3fe3ea6c985236f5b6ce8a90bb2ef3167151cf0e761142569")
+					  .addHeader("Content-Type", "application/json")
+					  .build();
+					Response response1 = client1.newCall(request1).execute();
+			
+					System.out.println(response1.body().string());
 			System.out.println(response.body().string());
 		}
 		return null;
 
 	}
 
-//	public static void main(String[] args) throws IOException {
-//		Map<String,Object> map=new HashMap<>();
-//		map.put("pan_id", "BVJPG6268H");
-//		map.put("name", "Neeraj Gaur");
-//		map.put("date_of_birth", "1989-12-25");
-//		map.put("consent", "Y");
-//		getDetails(map);
-//	}
+	public static void main(String[] args) throws IOException {
+		Map<String,Object> map=new HashMap<>();
+		map.put("pan_id", "BVJPG6268H");
+		map.put("name", "Neeraj Gaur");
+		map.put("date_of_birth", "1989-12-25");
+		map.put("consent", "Y");
+		getDetails(map);
+	}
 	
 
 	@Override
